3a2cb056f872cde13e277b9336177a8a48032c1d
Augusto <augustomarengo@hotmail.com>
2023-01-30 21:45:43 -0300

    Cambios

--- src/options/db.js
+++ src/options/db.js
@@ -1,13 +1,4 @@
 export const dbOptions = {
-    mysql:{
-        client: "mysql2",
-    connection:{
-        host:'localhost',
-        user:'root',
-        password:'',
-        database:'entrega07'
-    }
-    },
     sqlite:{
         client:'sqlite3',
     connection:{
